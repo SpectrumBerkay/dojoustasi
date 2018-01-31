2cdcbf493c7aad106d08a9306cf56b7bbbe7fe51
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-31 21:07:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,52 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-await new ModernEmbedBuilder
-{
-    Content = ("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```"),
-    Title = "title ~~(did you know you can have markdown here too?)~~",
-    Description = "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
-    Url = "https://discordapp.com",
-    Color = 0xD3CF13,
-    Timestamp = "2018-01-31T17:52:18.547Z",
-    Footer = ("footer text", "https://cdn.discordapp.com/embed/avatars/0.png"),
-    ThumbnailUrl = "https://cdn.discordapp.com/embed/avatars/0.png",
-    ImageUrl = "https://cdn.discordapp.com/embed/avatars/0.png",
-    Author = ("author name", "https://discordapp.com", "https://cdn.discordapp.com/embed/avatars/0.png"),
-    Fields = 
+const embed = {
+  "title": "title ~~(did you know you can have markdown here too?)~~",
+  "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+  "url": "https://discordapp.com",
+  "color": 13881107,
+  "timestamp": "2018-01-31T17:52:18.547Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+    "text": "footer text"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "author": {
+    "name": "author name",
+    "url": "https://discordapp.com",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [
     {
-        ("🤔", "some of these properties have certain limits...")
-        ("😱", "try exceeding some of them!")
-        ("🙄", "an informative error should show up, and this view will remain as-is until all issues are fixed")
-        ("<:thonkang:219069250692841473>", "these last two", inline: true)
-        ("<:thonkang:219069250692841473>", "are inline fields", inline: true)
+      "name": "🤔",
+      "value": "some of these properties have certain limits..."
+    },
+    {
+      "name": "😱",
+      "value": "try exceeding some of them!"
+    },
+    {
+      "name": "🙄",
+      "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "these last two",
+      "inline": true
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "are inline fields",
+      "inline": true
     }
-}.Send(message);
+  ]
+};
+channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
            
 client.login(process.env.BOT_TOKEN);
