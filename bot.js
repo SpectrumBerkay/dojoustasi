82114aea5d781216dc399089dc8464177802c089
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-31 18:43:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-var prefix = ("fbebsi");
+var prefix = ("$");
 
 client.on('ready', () => {
   console.log(`Bot Working....`);
 });
- 
+
+client.on("message", async message => {
+
+  if(command === '${prefix}userinfo') {
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setDescription("This is the user info!");
+    
+    message.channel.sendEmbed(embed)
+  }
+});  
 
 client.login(process.env.BOT_TOKEN);
